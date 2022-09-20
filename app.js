8a79a2f6eccd2e6acfebb3fa7afc4b77e9173391
rtezen23 <raul_tezen@hotmail.com>
2022-09-20 16:01:24 -0500

    carpeta public en backend

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 var cors = require('cors');
+const path = require('path');
 
 const { buzonRouter } = require('./routes/buzon.routes');
+const { viewsRouter } = require('./routes/views.routes');
 
 const app = express();
 
 app.use(cors());
 
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
+
 app.use('/api/v1/buzon', buzonRouter);
+app.use('/*', viewsRouter);
 
 module.exports = { app };   
